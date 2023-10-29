01d24c9ec87a3fca42974825cf4423f20285acc0
imranhosain6554 <imran122.office@gmail.com>
2023-10-29 14:12:20 +0600

    first commit setup

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Shared/Navbar/Navbar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default HomePage;
