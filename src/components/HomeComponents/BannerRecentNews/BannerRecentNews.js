0e1073b8463adedea7f5d862b6f7e8afad63d23b
imranhosain6554 <imran122.office@gmail.com>
2023-11-28 10:12:59 +0600

    fix design and dynamic

--- src/components/HomeComponents/BannerRecentNews/BannerRecentNews.js
+++ src/components/HomeComponents/BannerRecentNews/BannerRecentNews.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 const BannerRecentNews = ({ data }) => {
   if (!data || data.length === 0) {
     return null; // Handle case when data is empty or not provided
@@ -7,84 +8,98 @@ const BannerRecentNews = ({ data }) => {
 
   const mainNews = data[0];
   const sideNews = data.slice(1, 4);
-  return (
-    <div className=" grid lg:grid-cols-2 lg:gap-x-32  lg:my-6 grid-cols-none">
-      <div className="">
-        <div>
-          <Image
-            className="w-full"
-            width={0}
-            height={0}
-            src={mainNews.newsImage}
-            alt="img"
-          ></Image>
-          <h5 className="text-center lg:text-[18px] text-[12px]">
-            {mainNews.newsTitle} ছবি : সংগৃহীত
-          </h5>
-        </div>
-        <div className=" px-1">
-          <Link href="/news-details">
-            <h2 className="font-bold lg:text-[30px] text-[16px] font-noto-sans-bengali mt-6 mb-4">
-              {mainNews.newsTitle}
-            </h2>
-          </Link>
-          <p
-            dangerouslySetInnerHTML={{
-              __html: mainNews.newsDetailsText.slice(0, 300),
-            }}
-            className="font-medium lg:text-[20px] text-[14px] mb-4 font-noto-sans-bengali text-justify"
-          ></p>
-        </div>
-        <Link href="/news-details">
-          <button type="button" className=" sm:m-4 m-4 hover:text-[#be5959]">
-            বিস্তারিত...
-          </button>
-        </Link>
-      </div>
-      {/* side data */}
-      <div className="">
-        {sideNews.map((news, index) => (
-          <div key={index}>
-            <div className="flex gap-2 mt-0">
-              <div>
-                <Image
-                  className="sm:w-[470px] sm:h-[174px] w-[900px] h-full"
-                  src={news.newsImage}
-                  width={0}
-                  height={0}
-                  alt="image"
-                ></Image>
-              </div>
-              <div className="w-100  items-start">
-                <Link href="/news-details">
-                  <h2 className="font-bold text-[20px] mt-7">
-                    {news.newsTitle}
-                  </h2>
-                </Link>
+  const [isClient, setIsClient] = useState(false);
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
-                <p
-                  className="font-normal text-[16px]  font-noto-sans-bengali"
-                  dangerouslySetInnerHTML={{
-                    __html: news.newsDetailsText.slice(0, 100),
-                  }}
-                ></p>
-                <button
-                  type="button"
-                  className=" font-semibold mb-4 mt-4 hover:text-[#be5959]"
-                >
-                  বিস্তারিত...
-                </button>
-              </div>
+  return (
+    <>
+      {isClient ? (
+        <div className=" grid lg:grid-cols-2 lg:gap-x-32  lg:my-6 grid-cols-none">
+          <div className="">
+            <div>
+              <Image
+                className="w-full"
+                width={0}
+                height={0}
+                src={mainNews.newsImage}
+                alt="img"
+              ></Image>
+              <h5 className="text-center lg:text-[18px] text-[12px]">
+                {mainNews.newsTitle} ছবি : সংগৃহীত
+              </h5>
             </div>
-            {index === 2 ? (
-              <div className=""></div>
-            ) : (
-              <div className="h-1 w-100 border-b-[1px] border-black mt-2"></div>
-            )}
+            <div className=" px-1">
+              <Link href="/news-details">
+                <h2 className="font-bold lg:text-[30px] text-[16px] font-noto-sans-bengali mt-6 mb-4">
+                  {mainNews.newsTitle}
+                </h2>
+              </Link>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: mainNews?.newsDetailsText.slice(0, 200),
+                }}
+                className="font-medium lg:text-[20px] text-[14px] mb-4 font-noto-sans-bengali text-justify"
+              />
+            </div>
+            <Link href="/news-details">
+              <button
+                type="button"
+                className=" sm:m-4 m-4 hover:text-[#be5959]"
+              >
+                বিস্তারিত...
+              </button>
+            </Link>
           </div>
-        ))}
-      </div>
-    </div>
+          {/* side data */}
+          <div className="">
+            {sideNews.map((news, index) => (
+              <div key={index}>
+                <div className="flex gap-2 mt-0">
+                  <div>
+                    <Image
+                      className="sm:w-[470px] sm:h-[174px] w-[900px] h-full"
+                      src={news.newsImage}
+                      width={0}
+                      height={0}
+                      alt="image"
+                    ></Image>
+                  </div>
+                  <div className="w-100  items-start">
+                    <Link href="/news-details">
+                      <h2 className="font-bold text-[20px] mt-7">
+                        {news.newsTitle}
+                      </h2>
+                    </Link>
+
+                    <p
+                      className="font-normal text-[16px]  font-noto-sans-bengali"
+                      dangerouslySetInnerHTML={{
+                        __html: news?.newsDetailsText.slice(0, 100),
+                      }}
+                    ></p>
+                    <button
+                      type="button"
+                      className=" font-semibold mb-4 mt-4 hover:text-[#be5959]"
+                    >
+                      বিস্তারিত...
+                    </button>
+                  </div>
+                </div>
+                {index === 2 ? (
+                  <div className=""></div>
+                ) : (
+                  <div className="h-1 w-100 border-b-[1px] border-black mt-2"></div>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        "hello"
+      )}
+    </>
   );
 };
 
